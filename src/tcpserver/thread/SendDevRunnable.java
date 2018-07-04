344b26d4924658e8799eef96e7f9a1f193398111
YangKuan <YangKuan@corpmain.com>
2018-07-04 09:15:35 +0800

    ..

--- src/tcpserver/thread/SendDevRunnable.java
+++ src/tcpserver/thread/SendDevRunnable.java
@@ -24,21 +24,22 @@ public class SendDevRunnable implements Runnable {
 	public void run() {
 		TcpServer tcpServer = new TcpServer();
 		int count = 0;
-		for(;;) {
+		for (;;) {
 			try {
 				count++;
 				System.out.println("第" + count + "次发送数据");
-				for(String sendString : sendDevStrArray) {
+				for (String sendString : sendDevStrArray) {
 					Thread.sleep(1000);
 					tcpServer.sendDataClient(socketChannel, sendString);
 				}
-				//休眠5秒
+				// 休眠5秒
 				Thread.sleep(10000);
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			//发送5次之后退出线程
-			if(count == 100) {
+			// 发送5次之后退出线程
+			if (count == 100) {
+				System.out.println("线程退出");
 				return;
 			}
 		}
